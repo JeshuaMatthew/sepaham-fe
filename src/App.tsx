bd0d16042929760e90ee1acc2db895366d0fa19b
Poda <poda.alvien@adidata.co.id>
2026-07-15 16:37:32 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-slate-100 min-h-screen">
+      <h1 className="font-bold text-blue-600 text-5xl">
+        Deglease 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
